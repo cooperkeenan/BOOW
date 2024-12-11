39292709ad6b299b67912288a21ae231be7cb084
cooperlkeenan <cooperlkeenan@gmail.com>
2024-12-11 21:29:36 +0000

    new obstacle

--- src/levels/level_2.cpp
+++ src/levels/level_2.cpp
@@ -10,6 +10,7 @@
 std::vector<Obstacle> level2_obstacle_1();
 std::vector<Obstacle> level2_obstacle_2();
 std::vector<Obstacle> level3_obstacle_3();
+std::vector<Obstacle> level4_obstacle_4();
 std::vector<Obstacle> level_1();
 
 
@@ -40,10 +41,21 @@ std::vector<Obstacle> level2_obstacle_2() {
 std::vector<Obstacle> level2_obstacle_3() {
     std::vector<Obstacle> obstacleSet;
     obstacleSet.push_back(curve(0,0, 20, 15));
-    obstacleSet.push_back(gap());
-    obstacleSet.push_back(curve(0, 0, 20, -10, false)); // Downward outward curve (convex)
-    obstacleSet.push_back(curve(5, -30, 20, -10, true));  // Downward inward curve (concave)
+    obstacleSet.push_back(gap(5));
+    obstacleSet.push_back(curve(0, 0, 20, -10, false)); 
+    obstacleSet.push_back(curve(5, -30, 20, -10, true));  
+    obstacleSet.push_back(line(0, -15, 10));
+    return obstacleSet;
+}
 
+// Obstacle 4
+std::vector<Obstacle> level2_obstacle_4() {
+    std::vector<Obstacle> obstacleSet;
+    obstacleSet.push_back(ramp(0, 0, 20, 20));
+    obstacleSet.push_back(gap(10));
+    obstacleSet.push_back(line(0,-10, 10));
+    obstacleSet.push_back(gap(10));
+    obstacleSet.push_back(line(0,-15, 30));
     return obstacleSet;
 }
 
@@ -60,11 +72,13 @@ std::vector<Obstacle> level_2() {
     auto part1 = level2_obstacle_1();
     auto part2 = level2_obstacle_2();
     auto part3 = level2_obstacle_3();
+    auto part4 = level2_obstacle_4();
 
     // Concatenate Obstacles
     level.insert(level.end(), part1.begin(), part1.end());
     level.insert(level.end(), part2.begin(), part2.end());
     level.insert(level.end(), part3.begin(), part3.end());
+    level.insert(level.end(), part4.begin(), part4.end());
 
     return level;
 }
